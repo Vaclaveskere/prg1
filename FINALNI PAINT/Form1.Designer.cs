de1e60f430e11f3425ab9ab4ffb99360b322ac9d
Vaclaveskere <144894657+Vaclaveskere@users.noreply.github.com>
2024-03-23 17:51:12 +0100

    Add files via upload

---
+++ FINALNI PAINT/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace PAINT_FINAL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Colors = new System.Windows.Forms.Button();
+            this.Pen = new System.Windows.Forms.Button();
+            this.Eraser = new System.Windows.Forms.Button();
+            this.EraseAll = new System.Windows.Forms.Button();
+            this.Background = new System.Windows.Forms.Button();
+            this.AddImage = new System.Windows.Forms.Button();
+            this.NewCanvas = new System.Windows.Forms.Button();
+            this.Thickness = new System.Windows.Forms.TrackBar();
+            this.Ellipse = new System.Windows.Forms.RadioButton();
+            this.Rectangle = new System.Windows.Forms.RadioButton();
+            this.Line = new System.Windows.Forms.RadioButton();
+            this.Triangle = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.Thickness)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 77);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1486, 750);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // Colors
+            // 
+            this.Colors.Location = new System.Drawing.Point(26, 12);
+            this.Colors.Name = "Colors";
+            this.Colors.Size = new System.Drawing.Size(75, 45);
+            this.Colors.TabIndex = 1;
+            this.Colors.Text = "Colors";
+            this.Colors.UseVisualStyleBackColor = true;
+            this.Colors.Click += new System.EventHandler(this.Colors_Click);
+            // 
+            // Pen
+            // 
+            this.Pen.Location = new System.Drawing.Point(137, 12);
+            this.Pen.Name = "Pen";
+            this.Pen.Size = new System.Drawing.Size(75, 23);
+            this.Pen.TabIndex = 2;
+            this.Pen.Text = "Pen";
+            this.Pen.UseVisualStyleBackColor = true;
+            this.Pen.Click += new System.EventHandler(this.Pen_Click);
+            // 
+            // Eraser
+            // 
+            this.Eraser.Location = new System.Drawing.Point(137, 41);
+            this.Eraser.Name = "Eraser";
+            this.Eraser.Size = new System.Drawing.Size(75, 23);
+            this.Eraser.TabIndex = 3;
+            this.Eraser.Text = "Eraser";
+            this.Eraser.UseVisualStyleBackColor = true;
+            this.Eraser.Click += new System.EventHandler(this.Eraser_Click);
+            // 
+            // EraseAll
+            // 
+            this.EraseAll.Location = new System.Drawing.Point(218, 12);
+            this.EraseAll.Name = "EraseAll";
+            this.EraseAll.Size = new System.Drawing.Size(75, 23);
+            this.EraseAll.TabIndex = 4;
+            this.EraseAll.Text = "Erase all";
+            this.EraseAll.UseVisualStyleBackColor = true;
+            this.EraseAll.Click += new System.EventHandler(this.EraseAll_Click);
+            // 
+            // Background
+            // 
+            this.Background.Location = new System.Drawing.Point(218, 41);
+            this.Background.Name = "Background";
+            this.Background.Size = new System.Drawing.Size(75, 23);
+            this.Background.TabIndex = 5;
+            this.Background.Text = "Background";
+            this.Background.UseVisualStyleBackColor = true;
+            this.Background.Click += new System.EventHandler(this.Background_Click);
+            // 
+            // AddImage
+            // 
+            this.AddImage.Location = new System.Drawing.Point(461, 12);
+            this.AddImage.Name = "AddImage";
+            this.AddImage.Size = new System.Drawing.Size(75, 23);
+            this.AddImage.TabIndex = 6;
+            this.AddImage.Text = "Add Image";
+            this.AddImage.UseVisualStyleBackColor = true;
+            this.AddImage.Click += new System.EventHandler(this.AddImage_Click);
+            // 
+            // NewCanvas
+            // 
+            this.NewCanvas.Location = new System.Drawing.Point(461, 41);
+            this.NewCanvas.Name = "NewCanvas";
+            this.NewCanvas.Size = new System.Drawing.Size(75, 23);
+            this.NewCanvas.TabIndex = 7;
+            this.NewCanvas.Text = "new canvas";
+            this.NewCanvas.UseVisualStyleBackColor = true;
+            this.NewCanvas.Click += new System.EventHandler(this.NewCanvas_Click);
+            // 
+            // Thickness
+            // 
+            this.Thickness.Location = new System.Drawing.Point(308, 26);
+            this.Thickness.Name = "Thickness";
+            this.Thickness.Size = new System.Drawing.Size(133, 45);
+            this.Thickness.TabIndex = 8;
+            this.Thickness.Scroll += new System.EventHandler(this.Thickness_Scroll);
+            // 
+            // Ellipse
+            // 
+            this.Ellipse.AutoSize = true;
+            this.Ellipse.Location = new System.Drawing.Point(559, 15);
+            this.Ellipse.Name = "Ellipse";
+            this.Ellipse.Size = new System.Drawing.Size(55, 17);
+            this.Ellipse.TabIndex = 0;
+            this.Ellipse.TabStop = true;
+            this.Ellipse.Text = "Ellipse";
+            this.Ellipse.UseVisualStyleBackColor = true;
+            
+            // 
+            // Rectangle
+            // 
+            this.Rectangle.AutoSize = true;
+            this.Rectangle.Location = new System.Drawing.Point(559, 47);
+            this.Rectangle.Name = "Rectangle";
+            this.Rectangle.Size = new System.Drawing.Size(74, 17);
+            this.Rectangle.TabIndex = 10;
+            this.Rectangle.TabStop = true;
+            this.Rectangle.Text = "Rectangle";
+            this.Rectangle.UseVisualStyleBackColor = true;
+            
+            // 
+            // Line
+            // 
+            this.Line.AutoSize = true;
+            this.Line.Location = new System.Drawing.Point(658, 18);
+            this.Line.Name = "Line";
+            this.Line.Size = new System.Drawing.Size(45, 17);
+            this.Line.TabIndex = 11;
+            this.Line.TabStop = true;
+            this.Line.Text = "Line";
+            this.Line.UseVisualStyleBackColor = true;
+            
+            // 
+            // Triangle
+            // 
+            this.Triangle.AutoSize = true;
+            this.Triangle.Location = new System.Drawing.Point(658, 47);
+            this.Triangle.Name = "Triangle";
+            this.Triangle.Size = new System.Drawing.Size(63, 17);
+            this.Triangle.TabIndex = 12;
+            this.Triangle.TabStop = true;
+            this.Triangle.Text = "Triangle";
+            this.Triangle.UseVisualStyleBackColor = true;
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(1486, 827);
+            this.Controls.Add(this.Triangle);
+            this.Controls.Add(this.Line);
+            this.Controls.Add(this.Rectangle);
+            this.Controls.Add(this.Ellipse);
+            this.Controls.Add(this.Thickness);
+            this.Controls.Add(this.NewCanvas);
+            this.Controls.Add(this.Colors);
+            this.Controls.Add(this.AddImage);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Background);
+            this.Controls.Add(this.Pen);
+            this.Controls.Add(this.EraseAll);
+            this.Controls.Add(this.Eraser);
+            this.MaximumSize = new System.Drawing.Size(2000, 2000);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Thickness)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button NewCanvas;
+        private System.Windows.Forms.Button AddImage;
+        private System.Windows.Forms.Button Background;
+        private System.Windows.Forms.Button EraseAll;
+        private System.Windows.Forms.Button Colors;
+        private System.Windows.Forms.Button Pen;
+        private System.Windows.Forms.Button Eraser;
+        private System.Windows.Forms.TrackBar Thickness;
+        private System.Windows.Forms.RadioButton Ellipse;
+        private System.Windows.Forms.RadioButton Rectangle;
+        private System.Windows.Forms.RadioButton Line;
+        private System.Windows.Forms.RadioButton Triangle;
+    }
+}
+
---
+++ FINALNI PAINT/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PAINT_FINAL
+{
+
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        int x = -1;
+        int y = -1;
+        bool moving = false;
+        Pen pen;
+        Pen pen1;
+        private PictureBox pictureBox;
+        private Color drawingColor = Color.Black;
+        private int penThickness = 5;
+        private List<PictureBox> pictureBoxes = new List<PictureBox>();
+        private bool isDragging = false;
+        private Point lastMousePosition;
+        private PictureBox selectedPictureBox = null;
+        bool PenClicked = false;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel1.CreateGraphics();
+            
+        }
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            
+            pen1 = new Pen(Color.Black,5);
+            
+            int width, height;
+            width = Math.Abs(e.X - x);
+            height = Math.Abs(e.Y - y);
+            if (Ellipse.Checked  && moving && x != -1 && y != -1)
+            {  
+                PenClicked = false;
+                g.DrawEllipse(pen1, x, y, e.X - x, e.Y - y);         
+            }
+            else if (Rectangle.Checked && moving && x != -1 && y != -1 )
+            {
+                PenClicked = false;
+                g.DrawRectangle(pen1, x, y, e.X - x, e.Y - y);
+            }
+            else if (Line.Checked  && moving && x != -1 && y != -1)
+            {
+                PenClicked = false;
+                g.DrawLine(pen1, x, y, e.X, e.Y);
+            }
+            else if (Triangle.Checked  && moving && x != -1 && y != -1 ) 
+            {
+                PenClicked = false;
+                Point[] trianglePoints = new Point[]
+                    {
+                        new Point(x, y),        // Vertex 1
+                        new Point(e.X, e.Y),    // Vertex 2 (current mouse position)
+                        new Point(x, e.Y)       // Vertex 3 (same X coordinate as the starting point, Y coordinate of the current mouse position)
+                    };
+                // Draw the triangle
+                g.DrawPolygon(pen1, trianglePoints);
+            } 
+            moving = false;
+            x = -1;
+            y = -1;
+        }    
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (moving && x != -1 && y != -1)
+            {
+
+                if (PenClicked == true)
+                {
+                    g.DrawLine(pen, new Point(x, y), e.Location);
+                    x = e.X;
+                    y = e.Y;
+                }
+            }
+        }
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            moving = true;
+            x = e.X;
+            y = e.Y;         
+        }
+        private void Colors_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.Color = drawingColor; // Set initial color to the current drawing color
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Update drawing color
+                drawingColor = colorDialog.Color;
+                pen.Color = drawingColor;
+                pen1.Color = drawingColor;
+            }
+        }
+        private void Pen_Click(object sender, EventArgs e)
+        {
+            PenClicked = true;
+            pen = new Pen(drawingColor, penThickness);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            Ellipse.Checked = false;
+            Rectangle.Checked = false;
+            Line.Checked = false;
+            Triangle.Checked = false;
+        }
+        private void Eraser_Click(object sender, EventArgs e)
+        {
+            pen = new Pen(panel1.BackColor, penThickness);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+        }     
+        private void Background_Click(object sender, EventArgs e)
+        {
+            panel1.BackColor = drawingColor;
+        }
+        private void EraseAll_Click(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+        private void AddImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.jpg; *.jpeg; *.png; *.bmp)|*.jpg; *.jpeg; *.png; *.bmp|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Load the selected image file
+                    string filePath = openFileDialog.FileName;
+                    Image originalImage = Image.FromFile(filePath);
+
+                    int maxWidth = 500; // Set your desired maximum width
+                    int maxHeight = 500; // Set your desired maximum height                  
+                    // Calculate the new dimensions while preserving aspect ratio
+                    int newWidth, newHeight;
+                    if (originalImage.Width > originalImage.Height)
+                    {
+                        newWidth = maxWidth;
+                        newHeight = (int)((float)originalImage.Height / originalImage.Width * maxWidth);
+                    }
+                    else
+                    {
+                        newWidth = (int)((float)originalImage.Width / originalImage.Height * maxHeight);
+                        newHeight = maxHeight;
+                    }
+                    // Resize the image
+                    Image resizedImage = new Bitmap(originalImage, newWidth, newHeight);
+                    // Create a new PictureBox
+                    pictureBox = new PictureBox();
+                    pictureBox.SizeMode = PictureBoxSizeMode.AutoSize; // Adjust this as needed
+                    pictureBox.Image = resizedImage;
+                    // Add event handlers to enable dragging
+                    pictureBox.MouseDown += PictureBox_MouseDown;
+                    pictureBox.MouseMove += PictureBox_MouseMove;
+                    pictureBox.MouseUp += PictureBox_MouseUp;
+                    // Add the PictureBox to the form's controls collection
+                    Controls.Add(pictureBox);
+                    pictureBox.BringToFront(); // Bring the PictureBox to the front
+                    pictureBoxes.Add(pictureBox);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading image: " + ex.Message);
+                }
+            }
+        }
+        private void PictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            
+            isDragging = true;
+            lastMousePosition = e.Location;
+            selectedPictureBox = (PictureBox)sender;
+        }
+        private void PictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging && selectedPictureBox != null)
+            {
+                int deltaX = e.Location.X - lastMousePosition.X;
+                int deltaY = e.Location.Y - lastMousePosition.Y;
+
+                selectedPictureBox.Left += deltaX;
+                selectedPictureBox.Top += deltaY;
+            }
+        }
+        private void PictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = false;
+                selectedPictureBox = null;
+            }
+        }     
+        private void NewCanvas_Click(object sender, EventArgs e)
+        {
+            // Clear all drawings from the canvas
+            panel1.Refresh();
+            // Reset the background color of the canvas to its normal state
+            panel1.BackColor = SystemColors.Control;
+            // Remove all PictureBox controls from the form
+            foreach (Control control in Controls)
+            {
+                if (control is PictureBox)
+                {
+                    Controls.Remove(control);
+                    control.Dispose();
+                }
+            }
+        }       
+        private void Thickness_Scroll(object sender, EventArgs e)
+        {
+            penThickness = Thickness.Value;
+            pen.Width = penThickness;
+        }
+
+          
+    }
+}
